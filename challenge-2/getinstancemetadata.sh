6dea793d34f791c1dfd1ba4b33afa34003e7bf8f
libu <libukmr@gmail.com>
2024-12-03 21:56:46 -0700

    Delete challenge-2 directory

--- challenge-2/getinstancemetadata.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#echo "Enter meta data of an instance from the below list: $1"
-#echo "Examples: service-accounts disks licenses network-interfaces scheduling""
-curl -s "http://metadata.google.internal/computeMetadata/v1/instance/$1/?recursive=true&alt=json" -H "Metadata-Flavor: Google"| jq
